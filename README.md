55a2f5932757e6799b4feda1399af93fc0b539a6
spi971 <sandypierre@protonmail.com>
2023-09-29 13:38:50 +0200

    Modify REAME.md : mongo connection

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # gwadflix is a Clone built using React, Redux Toolkit, Firebase, Styled Components, Axios, Node.js, Express and MongoDB.
 
+## Mongodb 
+You should have mongodb on your computer or use [mongodb atlas](https://www.mongodb.com/atlas/database).
+
+You have to put your url to the mongoose connect function in ``` /back/server.js```
+
+your mongoo connection should look like this:
+```
+mongoose
+  .connect("<your url to your mongo database>", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Connected db`);
+  })
+  .catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+  }); 
+```
+
 ## Firebase 
 You should have an firebase account.
+
+
 Then you have to create a firebase config file.
 ```touch /ui/src/config/firebase.js ```
+
+
 The content of the file should lok like this
 ```
 import { initializeApp } from "firebase/app";
--- back/controllers/favorisController.js
+++ back/controllers/favorisController.js
@@ -4,12 +4,12 @@ module.exports.addToFavoris = async (req, res) => {
   try {
     const { email, data } = req.body;
     const favori = await Favoris.findOne({ email });
-    
+
     if (favori) {
       const { movies } = favori;
-      console.log(movies)
+      console.log(movies);
       const alreadyInList = movies.find(({ id }) => id === data.id);
-      console.log(alreadyInList)
+      console.log(alreadyInList);
       if (!alreadyInList) {
         await Favoris.findByIdAndUpdate(
           favori._id,
@@ -22,6 +22,23 @@ module.exports.addToFavoris = async (req, res) => {
     } else await Favoris.create({ email, movies: [data] });
     return res.json({ msg: "Movie successfully add to favoris list." });
   } catch (error) {
-    return res.json({ msg: "Error, can't add to favoris list" });
+    return res.json({ msg: "Error, can't add to favoris list." });
+  }
+};
+
+module.exports.getFavoris = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const favori = await Favoris.findOne({ email });
+    if (favori) {
+      return res.json({
+        msg: "Fecth favoris list success.",
+        movies: favori.movies,
+      });
+    } else {
+      return res.json({ msg: "Your favoris list is empty." });
+    }
+  } catch (error) {
+    return res.json({ msg: "Error, getting favoris list." });
   }
 };
